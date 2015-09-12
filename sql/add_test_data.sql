f01665a8a9f811b945bdf823837faf7ec4afd2be
eerojala <eero.ojala@helsinki.fi>
2015-09-12 22:37:34 +0300

    testitaulut lisätty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,61 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Operator (username, password) VALUES ('eero', '12345');
+INSERT INTO Operator (username, password) VALUES ('joq', '67879');
+
+INSERT INTO Competitor (competitorName, birthdate, country) VALUES ('eero', 
+DATE '1994-11-22', 'Suomi');
+INSERT INTO Competitor (competitorName, birthdate, country) VALUES ('joq', 7
+DATE '1111-11-11', 'Ruotsi');
+
+INSERT INTO Competition (competitionName, location, startsAt, endsAt, finished) 
+VALUES ('kisa1', 'Espoo, Suomi', TIMESTAMP '2015-09-12 12:00:00', 
+TIMESTAMP '2015-09-12 17:00:00', FALSE);
+
+INSERT INTO Competition (competitionName, location, startsAt, endsAt, finished) 
+VALUES ('kisa2', 'Mesta, Suomi', TIMESTAMP '1999-12-31 23:00:00',
+TIMESTAMP '2000-01-01 01:00:00', TRUE);
+
+INSERT INTO Participant (competitionId, competitorId, competitorNumber) VALUES(
+    (
+    SELECT id FROM Competition
+    WHERE competitionName = 'kisa2';
+    ),
+    (
+    SELECT id FROM Competitor
+    WHERE competitorName = 'eero';
+    ), 1
+);
+
+INSERT INTO Participant (competitionId, competitorId, competitorNumber) VALUES(
+    (
+    SELECT id FROM Competition
+    WHERE competitionName = 'kisa2';
+    ),
+    (
+    SELECT id FROM Competitor
+    WHERE competitorName = 'joq';
+    ), 2
+);
+
+INSERT INTO Results (participantId, participantNumber, firstSplit, secondSplit, 
+finalSplit, standing) VALUES(
+    (
+    SELECT id FROM Participant
+    WHERE participantNumber=1
+    ),
+    (
+    SELECT participantNumber FROM Participant
+    WHERE participantNumber = 1
+    ), TIME '00:27:16:01' , TIME '01:35:56:44', TIME '02:12:01:98', 1
+);
+
+INSERT INTO Results (participantId, participantNumber, firstSplit, secondSplit,
+finalSplit, standing) VALUES(
+    (
+    SELECT id FROM Participant
+    WHERE participantNumber=2
+    ),
+    (
+    SELECT participantNumber FROM Participant
+    WHERE participantNumber = 2
+    ), TIME '00:25:99:14', TIME '01:29:44:78', TIME '02:11:32:00', 2
+);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,38 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Operator(
+    id SERIAL PRIMARY KEY,
+    username varchar(50) NOT null,
+    password varchar(50) NOT null
+);
+
+CREATE TABLE Competitor(
+    id SERIAL PRIMARY KEY,
+    competitorName varchar(50) NOT null,
+    birthdate DATE,
+    country varchar(30) NOT null
+);
+
+CREATE TABLE Competition(
+    id SERIAL PRIMARY KEY,
+    competitionName varchar(100) NOT null,
+    location varchar(100) NOT null,
+    startsAt timestamp,
+    endsAt timestamp,
+    finished boolean
+);
+
+CREATE TABLE Participant(
+    id SERIAL PRIMARY KEY,
+    competitionId INTEGER REFERENCES Competition(id),
+    competitorId INTEGER REFERENCES Competitor(id),
+    participantNumber INTEGER
+);
+
+CREATE TABLE Results(
+    participantId INTEGER PRIMARY KEY,
+    participantNumber INTEGER,
+    firstSplit TIME,
+    secondSplit TIME,
+    finalSplit TIME,
+    standing INTEGER,
+    FOREIGN KEY (participantId, participantNumber) references Participant(id, participantNumber),
+);
